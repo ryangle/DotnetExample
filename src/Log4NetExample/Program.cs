966104a48ec51a9fcd11949fa0a3102ac2ca8b9a
ryangle <hcg1228@foxmail.com>
2019-04-23 17:17:33 +0800

    add log4net

--- src/Log4NetExample/Program.cs
+++ src/Log4NetExample/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Reflection;
 
 namespace Log4NetExample
 {
@@ -6,7 +9,12 @@ namespace Log4NetExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new System.IO.FileInfo("log4net.config"));
+            ILog log = LogManager.GetLogger(typeof(Program));
+
+            log.Info("------- Hello World! -------------------");
+
         }
     }
 }
