3a14ea35f95de4b8ae1938071d2b721087030aeb
ryangle <hcg1228@foxmail.com>
2021-05-13 17:02:24 +0800

    add ocelot sample

---
+++ src/Ocelot/Portal/Startup.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portal
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot(Configuration);
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("portal", new OpenApiInfo { Title = "Portal", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint($"/Service1/swagger/swagger.json", "Service1");
+                    c.SwaggerEndpoint("/swagger/portal/swagger.json", "Portal v1");
+                });
+            }
+
+            app.UseRouting();
+            app.UseOcelot();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //});
+        }
+    }
+}
--- src/PrintArguments/Program.cs
+++ src/PrintArguments/Program.cs
@@ -1,20 +1,40 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Timers;
 
 namespace PrintArguments
 {
     class Program
     {
+        static Timer t = new Timer();
         static void Main(string[] args)
         {
-            var mainexe = "D:\\PrintArgs\\PrintArguments.exe";
-            var processInfo = new ProcessStartInfo(mainexe)
-            {
-                WorkingDirectory = Path.GetDirectoryName(mainexe)
-            };
-            processInfo.Arguments = "-t xxx";
-            Process.Start(processInfo);
+            
+            t.Interval = 1000;
+            t.AutoReset = true;
+            //设置是否执行System.Timers.Timer.Elapsed事件
+            t.Enabled = true;
+            //绑定Elapsed事件
+            t.Elapsed += new System.Timers.ElapsedEventHandler(TimerUp);
+
+            t.Start();
+            Console.WriteLine($"Timer start {t.Enabled}");
+            Console.ReadLine();
+            t.Stop();
+            Console.WriteLine($"Timer stop {t.Enabled}");
+            Console.ReadLine();
+            t.Start();
+
+            Console.WriteLine($"Timer start {t.Enabled}");
+            Console.ReadLine();
+            //var mainexe = "D:\\PrintArgs\\PrintArguments.exe";
+            //var processInfo = new ProcessStartInfo(mainexe)
+            //{
+            //    WorkingDirectory = Path.GetDirectoryName(mainexe)
+            //};
+            //processInfo.Arguments = "-t xxx";
+            //Process.Start(processInfo);
 
             //Console.WriteLine("Print args");
             //if (args != null)
@@ -25,6 +45,16 @@ namespace PrintArguments
             //    }
             //}
             //Console.ReadLine();
+        }
+        /// <summary>
+        /// Timer类执行定时到点事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void TimerUp(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine($"TimerUp");
+            t.Stop();
         }
     }
 }
