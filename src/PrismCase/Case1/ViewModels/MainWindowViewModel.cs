84f8571f749eacfc20f099d7b0301f2a2bc6976a
ryangle <hcg1228@foxmail.com>
2022-04-11 13:21:47 +0800

    add PrismCase

---
+++ src/PrismCase/Case1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case1.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string? _windowTitle;
+        public string? WindowTitle
+        {
+            get { return _windowTitle; }
+            set { SetProperty(ref _windowTitle, value); }
+        }
+        public MainWindowViewModel()
+        {
+            WindowTitle = "Case1";
+        }
+    }
+}
---
+++ src/PrismCase/Case2/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Case2.Views;
+using Prism.DryIoc;
+using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Case2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
